9c57b5ea5150f4af337cb24dae56acb6684952dd
Ivan <ikduong100@student.hbuhsd.edu>
2024-12-09 19:20:13 +0000

    classwork

--- Main.java
+++ Main.java
@@ -4,6 +4,6 @@ public class Main
 {
   public static void main(String[] args)
   {
-
+    Point2D bllah = new Point2D();
   }
 }
--- Point2D.java
+++ Point2D.java
@@ -1,23 +1,23 @@
 import java.util.Scanner;
 
-public class Main
-{
-  public static void main(String[] args)
-  {
-    Point2D p1 = new Point2D();
-    Point2D p2 = new Point2D();
+// public class Main
+// {
+//   public static void main(String[] args)
+//   {
+//     Point2D p1 = new Point2D();
+//     Point2D p2 = new Point2D();
 
-    p1.setX(0);
-    p1.setY(1);
-    p1.showX();
-    p1.showY();
+//     p1.setX(0);
+//     p1.setY(1);
+//     p1.showX();
+//     p1.showY();
     
-    p2.setX(1);
-    p2.setY(0);
-    p2.showX();
-    p2.showY();
-  }
-}
+//     p2.setX(1);
+//     p2.setY(0);
+//     p2.showX();
+//     p2.showY();
+//   }
+// }
 
 public class Point2D
 {
